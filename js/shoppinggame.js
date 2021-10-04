618bec82a8c6d5887800ced15b6516195858a594
Jayme Coulton <coultonjayme@yahoo.com>
2021-10-04 16:44:35 -0500

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -28,7 +28,7 @@ function Product(id, name, price, expiryDate) {
 // Complete the dateDiff function
 const dateDiff = (date1, date2) => {
 	let timeDiff = Math.abs(date2.getTime() - date1.getTime());
-	let diffDays = Math.Ceil(timeDiff / (1000 * 3600 * 24));
+	let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 	return diffDays;
 };
 
