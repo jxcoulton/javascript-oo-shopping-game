7a50d59f84d15d54ec8884737d9cb540954320dd
Jayme Coulton <coultonjayme@yahoo.com>
2021-10-04 17:02:39 -0500

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -278,16 +278,20 @@ const findPointsToBill = (roundedTotal) => {
 
 // Complete this function
 const findPointsForExpDate = (prod) => {
-	if (prod.daysToExpire < 30) {
-		return 10;
-	} else {
-		return 0;
-	}
+	return prod.daysToExpire < 30 ? 10 : 0;
 };
 
 const calculatePoints = (prod, tBill) => {
 	let pointsToBill = findPointsToBill(Math.round(tBill));
 	let pointsForExpDate = findPointsForExpDate(prod);
+	player.score = player.score + pointsToBill + pointsForExpDate;
+	if (prod instanceof MagicProduct) {
+		if (prod.isBonus) {
+			player.addPoints(prod.points);
+		} else {
+			player.deductPoints(prod.points);
+		}
+	}
 };
 
 // Complete this function
